9ff2381dd6c076ca09f49d644b29d3e2000e9811
Ana Marr <acarolinasim@gmail.com>
2020-03-23 17:23:33 +1030

    dayArray

---
+++ script.js
@@ -0,0 +1,75 @@
+var dayArray = [
+    {
+        index: "0",
+        hour: "8",
+        time: "8",
+        meridiem: "am",
+        event: ""
+    },
+    {
+        index: "1",
+        hour: "9",
+        time: "9",
+        meridiem: "am",
+        event: ""
+
+    },
+    {
+        index: "2",
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        event: ""
+    },
+    {
+        index: "3",
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        event: ""
+    },
+    {
+        index: "4",
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        event: ""
+    },
+    {
+        index: "5",
+        hour: "1",
+        time: "1",
+        meridiem: "pm",
+        event: ""
+    },
+    {
+        index: "6",
+        hour: "2",
+        time: "2",
+        meridiem: "pm",
+        event: ""
+    },
+    {
+        index: "7",
+        hour: "3",
+        time: "3",
+        meridiem: "pm",
+        event: ""
+    },
+    {
+        index: "8",
+        hour: "4",
+        time: "4",
+        meridiem: "pm",
+        event: ""
+    },
+    {
+        index: "9",
+        hour: "5",
+        time: "5",
+        meridiem: "pm",
+        event: ""
+
+    }
+        
+]
